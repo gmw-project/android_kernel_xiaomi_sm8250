c32a5e9087b04b4afc51feb7f7279c4d9b613089
Danda <dandagaharu77@gmail.com>
2023-06-26 18:16:44 +0700

    remove unused dt2w symlinks

--- drivers/misc/symlink_oos.c
+++ drivers/misc/symlink_oos.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
 /*
- * OxygenOS DT2W interfaces
+ * OPlus DT2W interfaces
  *
  * Copyright (C) 2022, Rudi Setiyawan <diphons@gmail.com>
  */
@@ -14,52 +14,26 @@
 #include <linux/seq_file.h>
 #include <linux/irq.h>
 #include <linux/gpio.h>
-#ifdef CONFIG_ARCH_SDM845
-#include <linux/slab.h>
-#include <linux/input/tp_common.h>
-#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("diphons");
-MODULE_DESCRIPTION("oxygen os touch gesture");
+MODULE_DESCRIPTION("OPlus DT2W node link");
 MODULE_VERSION("0.0.9");
 
 #define tpdir "touchpanel"
 
 #define d_tap "touchpanel/double_tap_enable"
 #define tp_dt "/touchpanel/double_tap"
-#define tp_g "touchpanel/gesture_enable"
-#define tpg "tp_gesture"
 #define tpdir_fts "devices/platform/soc/a98000.i2c/i2c-3/3-0038/fts_gesture_mode"
 
-#ifdef CONFIG_ARCH_SDM845
-bool capacitive_keys_enabled;
-struct kobject *touchpanel_kobj;
-
-#define TS_ENABLE_FOPS(type)                                                   \
-	int tp_common_set_##type##_ops(struct tp_common_ops *ops)              \
-	{                                                                      \
-		static struct kobj_attribute kattr =                           \
-			__ATTR(type, (S_IWUSR | S_IRUGO), NULL, NULL);         \
-		kattr.show = ops->show;                                        \
-		kattr.store = ops->store;                                      \
-		return sysfs_create_file(touchpanel_kobj, &kattr.attr);        \
-	}
-
-TS_ENABLE_FOPS(capacitive_keys)
-TS_ENABLE_FOPS(double_tap)
-TS_ENABLE_FOPS(reversed_keys)
-#endif
-
-static int __init touch_oos_init(void) {
+static int __init oplus_dt2w_init(void) {
 	char *driver_path;
 	static struct proc_dir_entry *tp_dir;
 	static struct proc_dir_entry *tp_oos;
 	int ret = 0;
 
-	printk(KERN_INFO "oxygen os touch gesture initial");
+	printk(KERN_INFO "OPlus DT2W node link initial");
 
-#if defined(CONFIG_BOARD_XIAOMI_SM8250) || defined(CONFIG_MACH_XIAOMI_SM8250) || defined(CONFIG_ARCH_SM8150)
 	tp_dir = proc_mkdir(tpdir, NULL);
 	driver_path = kzalloc(PATH_MAX, GFP_KERNEL);
 	if (!driver_path) {
@@ -75,42 +49,17 @@ static int __init touch_oos_init(void) {
 
 	if (!tp_oos) {
 		ret = -ENOMEM;
-	}
-	kfree(driver_path);
-	tp_oos = proc_symlink(tp_g, NULL, "double_tap_enable");
-	if (!tp_oos)
-		ret = -ENOMEM;
-#else
-	tp_dir = proc_mkdir(tpdir, NULL);
-	touchpanel_kobj = kobject_create_and_add("touchpanel", NULL);
-	if (!touchpanel_kobj)
-		ret = -ENOMEM;
-
-	sprintf(driver_path, "/sys%s", tp_dt);
-	pr_err("%s: driver_path=%s\n", __func__, driver_path);
-	tp_oos = proc_symlink(tp_g, NULL, driver_path);
-	if (!tp_oos) {
-		ret = -ENOMEM;
-	}
-	kfree(driver_path);
-	tp_oos = proc_symlink(d_tap, NULL, "gesture_enable");
-	if (!tp_oos)
-		ret = -ENOMEM;
-#endif
-	tp_oos = proc_symlink(tpg, NULL, tp_g);
-	if (!tp_oos) {
-		ret = -ENOMEM;
-		printk(KERN_INFO "oxygen os touch gesture initial failed");
+		printk(KERN_INFO "OPlus DT2W node link failed");
 	} else
-		printk(KERN_INFO "oxygen os touch gesture initialized");
+		printk(KERN_INFO "OPlus DT2W node link initialized");
 
 	return ret;
 }
 
-static void __exit touch_oos_exit(void) {
-	printk(KERN_INFO "oxygen os touch gesture exit");
+static void __exit oplus_dt2w_exit(void) {
+	printk(KERN_INFO "OPlus DT2W node link exit");
 }
 
-module_init(touch_oos_init);
-module_exit(touch_oos_exit);
+module_init(oplus_dt2w_init);
+module_exit(oplus_dt2w_exit);
 
